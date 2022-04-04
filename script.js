0dfb783a2aa68e394e58c2a5c14de570ec8bfb73
Patrick Rallings <patrickrallings@Patricks-MacBook-Pro.local>
2022-04-04 17:58:53 -0400

    Switching ti imac

--- script.js
+++ script.js
@@ -1,6 +1,25 @@
-const submitBtn = $('#submit')
-const name = $('#name')
-const email = $('#email')
-const number = $('#phone')
-const message = $('#message-area')
+$("#submit").on("click", function () {
+  let name = $("#name");
+  let email = $("#email");
+  let number = $("#phone");
+  let required = [name, email, number];
 
+  for (let i = 0; i < required.length; i++) {
+    if (required[i].val() === "") {
+      $("p").text("Please Fill Out Required Fields");
+      $("p").addClass("warning");
+      required[i].prev().addClass("warning");
+    }
+    if (!(required[i].val() === "")) {
+      $("label").on("click", function () {
+        $("p").text("");
+        required[i].prev().removeClass("warning");
+      });
+    }
+  }
+
+  if (!$("label").hasClass("warning")) {
+    $("form").remove();
+    $("h2").text("Thanks for your feedback!");
+  }
+});
